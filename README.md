b86d35e8d4329a5b44e8842d6967df6a053b0916
zhangyang <zyayhj.yhj@163.com>
2016-08-10 19:52:36 +0800

    重要更新，升级到5.2.0。增加人机行为识别、设备指纹检测、数据传输加密等功能

--- README.md
+++ README.md
@@ -1,40 +1,50 @@
-# 点触验证码使用demo
+## 点触验证码 SDK (适配点触公钥版本 5.2.0 , 点触后台可查看)
 
 详情请关注各语言sdk
 
-[![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/JAVA4.png)](https://github.com/touclick/captcha-demo/tree/master/java-sdk)
-[![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/PHP4.png)](https://github.com/touclick/captcha-demo/tree/master/php-sdk)
-[![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/python4.png)](https://github.com/touclick/captcha-demo/tree/master/python-sdk)
-[![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/GO4.png)](https://github.com/touclick/captcha-demo/tree/master/go-sdk)
-[![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/JS4.png)](https://github.com/touclick/captcha-demo/tree/master/nodejs-demo)
+[![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/JAVA4.png)](https://github.com/touclick/captcha-demo/tree/master/java-sdk)
+[![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/PHP4.png)](https://github.com/touclick/captcha-demo/tree/master/php-sdk)
+[![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/python4.png)](https://github.com/touclick/captcha-demo/tree/master/python-sdk)
+[![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/GO4.png)](https://github.com/touclick/captcha-demo/tree/master/go-sdk)
+[![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/JS4.png)](https://github.com/touclick/captcha-demo/tree/master/nodejs-demo)
+
+### 适用情况
+
+使用该页面的SDK，请到 http://admin.touclick.com 注册新账号，或者联系点触工程师，升级原有账号。点触公钥版本要求大于 5.2.0
+
+### Js 地址
+
+点触 JavaScript Lib 地址均为云端地址, 不建议本地化; 请将该地址引用到需要添加点触验证码的页面中
+```html
+<!--公钥从http://www.touclick.com 注册获取-->
+<script type="text/javascript" src="//js.touclick.com/js.touclick?b=公钥" ></script>
+```
+### JS API 文档
+README-jslib.md 是 JavaScript Lib 文档 
+
+### 历史版本SDK
+
+`5.2.0` 当前版本
+
+`5.1.0` 不再提供新接入，点触客服人员会尽快联系开发者进行升级
 
 
-# JavaScript Lib 
+### 效果图
 
-点触 JavaScript Lib 地址均为云端地址, http://js.touclick.com/js.touclick?b=公钥 (从http://www.touclick.com 注册获取)
+拖动拼合
 
-使用该页面的SDK，请到 http://admin.touclick.com 注册新账号，或者联系点触工程师，升级原有账号。
-
-README-jslib.txt 是 JavaScript Lib 文档 
-
-如有疑问，请添加点触官方QQ群咨询：群号码：205658675
-
-
-# 效果图
-
-拖动旋转
-
-![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/rotate.png)
+![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/drag-3.png)
 
 图标点击
 
-![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/block.png)
+![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/block-14.png)
 
 图文点击
 
-![image](https://raw.githubusercontent.com/touclick/captcha-demo/master/images/classical_new.png)
+![image](https://raw.githubusercontent.com/zyayhj/captcha-demo/image/images/classical_13.png)
 
 
+### 联系我们：
+（商务洽谈）官Q1：3180210030 ，电话010-53608568
 
-
-
+（技术支持）官Q1：495067988  
--- java-sdk/captcha-java-sdk/src/main/java/com/touclick/captcha/service/TouClick.java
+++ java-sdk/captcha-java-sdk/src/main/java/com/touclick/captcha/service/TouClick.java
@@ -8,9 +8,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.apache.log4j.Logger;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.touclick.captcha.exception.TouclickException;
 import com.touclick.captcha.http.HttpClient;
@@ -35,31 +33,32 @@ public class TouClick implements Serializable {
 
     private static final long serialVersionUID = -176092625883595547L;
     private static final String HTTP = "http://";
-    private static final String POSTFIX = ".touclick.com/sverify.touclick";
+    private static final String CHECK_POSTFIX = ".touclick.com/sverify.touclick2";
+    private static final String CALLBACK_POSTFIX = ".touclick.com/callback";
 
     private HttpClient client = new HttpClient();
 
     /**
      * 请求二次验证, 服务端验证
      *
-     * @param checkCode 校验码，开发者自定义，一般采用手机号或者用户ID，用来更细致的频次控制
      * @param checkAddress  二次验证地址，二级域名
+     * @param sid session id
      * @param token     二次验证口令，单次有效
      * @param pubKey    公钥
      * @param priKey    私钥
      * @return Status   返回类型
      * @throws TouclickException
      */
-    public Status check(String checkCode, String checkAddress, String token,String pubKey,String priKey) throws TouclickException {
-        return this.check(checkCode, checkAddress, token, pubKey, priKey, "", "");
+    public Status check(String checkAddress, String sid,String token,String pubKey,String priKey) throws TouclickException {
+        return this.check(checkAddress,sid, token,pubKey, priKey, "", "");
     }
 
     /**
      * 请求二次验证, 服务端验证
      *
-     * @param checkCode 校验码，开发者自定义，一般采用手机号或者用户ID，用来更细致的频次控制
      * @param checkAddress  二次验证地址，二级域名
      * @param token     二次验证口令，单次有效
+     * @param sid session id
      * @param pubKey    公钥
      * @param priKey    私钥
      * @param userName  请求用户名 用于统计分析
@@ -67,24 +66,24 @@ public class TouClick implements Serializable {
      * @return Status    返回类型
      * @throws TouclickException
      */
-    public Status check(String checkCode, String checkAddress, String token,String pubKey,String priKey, String userName, String userId) throws TouclickException {
-        if (checkCode == null
-                || checkAddress == null || "".equals(checkAddress)
+    public Status check(String checkAddress, String sid,String token,String pubKey,String priKey, String userName, String userId) throws TouclickException {
+        if (checkAddress == null || "".equals(checkAddress)
                 || pubKey == null || "".equals(pubKey)
                 || priKey == null || "".equals(priKey)
-                || token == null || "".equals(token)) {
+                || token == null || "".equals(token)
+                || sid == null || "".equals(sid)) {
             throw new TouclickException("参数有误");
         }
         Pattern pattern = Pattern.compile("^[_\\-0-9a-zA-Z]+$");
         Matcher matcher = pattern.matcher(checkAddress);
         if(!matcher.matches()){
-            return new Status(Status.CHECKADDRESS_ERROR, Status.getCause(Status.CHECKADDRESS_ERROR));
+            return new Status(Status.CHECKADDRESS_ERROR,"0", Status.getCause(Status.CHECKADDRESS_ERROR));
         }
         
         List<Parameter> params = new ArrayList<Parameter>();
-        params.add(new Parameter("ckcode", checkCode));
         params.add(new Parameter("i", token));
         params.add(new Parameter("b", pubKey));
+        params.add(new Parameter("s",sid));
         try {
             params.add(new Parameter("ip", InetAddress.getLocalHost().getHostAddress()));
         } catch (UnknownHostException e) {
@@ -96,10 +95,9 @@ public class TouClick implements Serializable {
         params.add(new Parameter("ran", ran));
         String sign = TouclickUtil.buildMysign(params, priKey);
         StringBuilder url = new StringBuilder();
-        url.append(HTTP).append(checkAddress).append(POSTFIX);
+        url.append(HTTP).append(checkAddress).append(CHECK_POSTFIX);
         params.add(new Parameter("sign", sign));
         Response response = null;
-        System.out.println(url);
         try {
             response = client.get(url.toString(), params);
         } catch (TouclickException e1) {
@@ -114,18 +112,67 @@ public class TouClick implements Serializable {
                 if(result.getCode() == 0){
                     if(result.getSign() != null && !"".equals(result.getSign())
                 	        && result.getSign().equals(buildSign(result.getCode(),ran,priKey))){
-                        return new Status(result.getCode(),result.getMessage());
+                        return new Status(result.getCode(),result.getCkCode(),result.getMessage());
                     }else{
-    	      	        return new Status(Status.SIGN_ERROR, Status.getCause(Status.SIGN_ERROR));
+    	      	        return new Status(Status.SIGN_ERROR,result.getCkCode(), Status.getCause(Status.SIGN_ERROR));
                     }
                 }
-                return new Status(result.getCode(),result.getMessage());
+                return new Status(result.getCode(),result.getCkCode(),result.getMessage());
             } catch (Exception e) {
                 LOGGER.error("transfer json error ..", e);
             }
-            return new Status(Status.STATUS_JSON_TRANS_ERROR, Status.getCause(Status.STATUS_JSON_TRANS_ERROR));
+            return new Status(Status.STATUS_JSON_TRANS_ERROR,"0", Status.getCause(Status.STATUS_JSON_TRANS_ERROR));
         }
-        return new Status(Status.STATUS_HTTP_ERROR, Status.getCause(Status.STATUS_HTTP_ERROR));
+        return new Status(Status.STATUS_HTTP_ERROR,"0", Status.getCause(Status.STATUS_HTTP_ERROR));
+    }
+
+    /**
+     * 用户名密码校验后的回调方法
+     *
+     * @param checkAddress  二次验证地址，二级域名
+     * @param token     二次验证口令，单次有效
+     * @param sid session id
+     * @param isLoginSucc 用户名和密码是否校验成功
+     * @throws TouclickException
+     */
+    public void callback(String checkAddress,String sid, String token,String pubKey,String priKey,boolean isLoginSucc) throws TouclickException {
+        if (checkAddress == null || "".equals(checkAddress)
+                || token == null || "".equals(token)
+                || pubKey == null || "".equals(pubKey)
+                || priKey == null || "".equals(priKey)
+                || sid == null || "".equals(sid)) {
+            throw new TouclickException("参数有误");
+        }
+        Pattern pattern = Pattern.compile("^[_\\-0-9a-zA-Z]+$");
+        Matcher matcher = pattern.matcher(checkAddress);
+        if(!matcher.matches()){
+            return ;
+        }
+
+        List<Parameter> params = new ArrayList<Parameter>();
+        params.add(new Parameter("i", token));
+        params.add(new Parameter("b", pubKey));
+        params.add(new Parameter("s",sid));
+        try {
+            params.add(new Parameter("ip", InetAddress.getLocalHost().getHostAddress()));
+        } catch (UnknownHostException e) {
+
+        }
+        params.add(new Parameter("su", isLoginSucc ? "1" : "0"));
+        String ran = UUID.randomUUID().toString();
+        params.add(new Parameter("ran", ran));
+
+        String sign = TouclickUtil.buildMysign(params, priKey);
+        params.add(new Parameter("sign", sign));
+
+        StringBuilder url = new StringBuilder();
+        url.append(HTTP).append(checkAddress).append(CALLBACK_POSTFIX);
+
+        try {
+        	client.get(url.toString(), params);
+        } catch (TouclickException e1) {
+            LOGGER.error(e1.getMessage());
+        }
     }
 
     private String buildSign(int code, String ran, String priKey) {
